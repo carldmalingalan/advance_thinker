38e47f9e8cbffcef1428b47fb3e89e40af6eb214
Teffany Mae M. Reyes <teffanymaereyes@gmail.com>
2018-07-01 15:53:38 +0800

    nice nice

---
+++ Delta/footer.php
@@ -0,0 +1,16 @@
+
+
+		<!-- Footer -->
+		<footer class="site-footer">
+			<p class="copyright">© Powered by ARCEB 2019</p>
+		</footer>
+		
+		<!-- JS -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/plugins.js"></script>
+	
+		<script type="text/javascript" src="js/main.js"></script>
+		
+	</body>
+</html>
---
+++ Delta/index.php
@@ -0,0 +1,222 @@
+
+
+
+<?php include ('header.php') ?>
+
+		<div class="main-container">
+							
+			<!-- Home Section -->
+			<section id="home" class="section home on-menu fullscreen-element">
+				<div class="section-container">
+					<div class="element-table">
+						<div class="table-cell">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-12 text-center">		
+										<div class="flexslider textslider bottom-margin animated" data-animation="fadeInUp" data-animation-delay="700">
+											<ul class="slides">
+												<li>
+													<h1>Welcome!</h1>
+												</li>
+												<li>
+													<h1>We're glad to have you.</h1>
+												</li>
+											</ul>
+										</div><!-- .fleslider -->
+
+										<div class="buttons-group animated" data-animation="fadeInUp" data-animation-delay="900">
+											<a href="#step1" class="button button-border button-white move-to" data-section-background="image-3">Log In</a>
+										</div>
+									</div><!-- .col-md-12 -->
+								</div><!-- .row -->
+							</div><!-- .container -->
+						</div><!-- .table-cell -->
+					</div><!-- .element-table -->
+				</div><!-- .section-container -->
+			</section><!-- #home -->
+
+			<!-- Step 1 Section -->
+			<section id="step1" class="section on-menu fullscreen-element">	
+				<div class="section-container">
+					<div class="element-table">
+						<div class="table-cell">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-12 text-center">
+										<div class="section-header animated" data-animation="fadeInUp" data-animation-delay="700">
+											<h2 class="section-title">How may I assist you?</h2>
+											<div class="line-separate line-white line-center"><span></span></div>
+										</div><!-- .section-header -->
+
+										<div class="animated" data-animation="fadeInUp" data-animation-delay="800">
+											<div class="text-center">
+												<h4>I am
+											    <select class="form-control">
+													  <option>a visitor</option>
+													  <option>a contractor</option>
+													  <option>making a delivery</option>
+													  <option>signing out</option>
+													</select><!-- .form-control -->
+												.</h4>
+											</div><!-- .text-center -->
+										</div><!-- .animated -->
+									</div><!-- .col-md-12 -->
+
+									</div>
+								</div><!-- .row -->
+							</div><!-- .container -->
+						</div><!-- .table-cell -->
+					</div><!-- .element-table -->
+				</div><!-- .section-container -->
+			</section><!-- #step1 -->
+			
+			<!-- Step 2 Section -->
+			<section id="step2" class="section on-menu fullscreen-element">
+				<div class="section-container">
+					<div class="element-table">
+						<div class="table-cell">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-10 col-md-offset-1 text-center">
+
+										<div class="section-header animated" data-animation="fadeInUp" data-animation-delay="700">
+											<h2 class="section-title">Tell us about yourself.</h2>
+											<div class="line-separate line-white line-center"><span></span></div>
+										</div><!-- .section-header -->
+
+										<div class="contact-forn animated" data-animation="fadeInUp" data-animation-delay="1100">
+											<form id="cf" name="cf" action="#" method="post">
+												<div class="form-process"></div>
+												<div class="row">
+													<div class="col-md-6">
+														<input type="text" id="cf-name" name="cf-name" placeholder="Full Name" class="required">
+													</div><!-- .col-md-6 -->
+													<div class="col-md-6">
+														<input type="email" id="cf-email" name="cf-email" placeholder="Email address" class="contact-form-email required">
+													</div><!-- .col-md-6 -->
+													<div class="col-md-6">
+														<input type="text" id="cf-company" name="cf-company" placeholder="Company" class="required">
+													</div><!-- .col-md-6 -->
+													<div class="col-md-6">
+														<input type="text" id="cf-phone" name="cf-purpose" placeholder="Purpose" class="required">
+													</div><!-- .col-md-6 -->
+												</div><!-- .row -->
+												<button class="button button-border button-white" type="submit" id="cf-submit" name="cf-submit" value="submit">Done</button>
+											</form><!-- #cf -->
+										</div><!-- .contact-forn -->
+										
+									</div><!-- .col-md-10 -->
+								</div><!-- .row -->
+							</div><!-- .container -->
+						</div><!-- .table-cell -->
+					</div><!-- .element-table -->
+				</div><!-- .section-container -->
+			</section><!-- #step2 -->
+			
+			<!-- Step 3 Section -->
+			<section id="step3" class="section on-menu fullscreen-element">
+				<div class="section-container">
+					<div class="element-table">
+						<div class="table-cell">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-12 text-center">					
+										<div class="section-header bottom-padding animated" data-animation="fadeInUp" data-animation-delay="700">
+											<h2 class="section-title">Read and Accept</h2>
+											<div class="line-separate line-white line-center"><span></span></div>
+											<div class="section-content text-justify">
+												<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.</p>
+											</div><!-- .section-content -->
+										</div><!-- .section-header -->
+									</div><!-- .col-md-12 -->
+								</div><!-- .row -->
+							</div><!-- .container -->
+						</div><!-- .table-cell -->
+					</div><!-- .element-table -->
+				</div><!-- .section-container -->
+			</section><!-- #step3 -->
+
+			<!-- Step 4 Section -->
+			<section id="step4" class="section on-menu fullscreen-element">
+				<div class="section-container">
+					<div class="element-table">
+						<div class="table-cell">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-12 text-center">
+										<div class="section-header bottom-padding animated" data-animation="fadeInUp" data-animation-delay="700">
+											<h2 class="section-title">Take a picture</h2>
+											<div class="line-separate line-white line-center"><span></span></div>
+											<h3>, smile for the camera</h3>
+										</div><!-- .section-header -->
+									</div><!-- .col-md-12 -->
+								</div><!-- .row -->
+							</div><!-- .container -->
+						</div><!-- .table-cell -->
+					</div><!-- .element-table -->
+				</div><!-- .section-container -->
+			</section><!-- #step4 -->
+
+			<!-- Step 5 Section -->
+			<section id="step5" class="section on-menu fullscreen-element">
+				<div class="section-container">
+					<div class="element-table">
+						<div class="table-cell">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-12 text-center">
+										<div class="section-header bottom-padding animated" data-animation="fadeInUp" data-animation-delay="700">
+											<div class="row">
+												<div class="col-md-6">
+												</div><!-- .col-md-6 -->
+												<div class="col-md-6 text-left">
+													<h3>I am </h3>
+													<h3>My name is </h3>
+													<h3>I am from </h3>
+												</div><!-- .col-md-6 -->
+											</div><!-- .row -->
+											<div class="buttons-group animated" data-animation="fadeInUp" data-animation-delay="900">
+												<a href="#done" class="button button-border button-white move-to" data-section-background="image-3">Looks good, sign me in!</a>
+											</div><!-- .buttons-group -->
+										</div><!-- .section-header -->
+									</div><!-- .col-md-12 -->
+								</div><!-- .row -->
+							</div><!-- .container -->
+						</div><!-- .table-cell -->
+					</div><!-- .element-table -->
+				</div><!-- .section-container -->
+			</section><!-- #step5 -->
+
+
+			<!-- Done Section -->
+			<section id="done" class="section on-menu fullscreen-element">
+				<div class="section-container">
+					<div class="element-table">
+						<div class="table-cell">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-10 col-md-offset-1 text-center">
+										<div class="section-header no-padding animated" data-animation="fadeInUp" data-animation-delay="700">
+											<h2 class="section-title">Signed In!</h2>
+											<div class="line-separate line-white line-center"><span></span></div>
+											<h3>Enjoy your visit. Have a nice day.</h3>
+										</div><!-- .section-header -->
+									</div><!-- .col-md-10 -->
+								</div><!-- .row -->
+							</div><!-- .container -->
+						</div><!-- .table-cell -->
+					</div><!-- .element-table -->
+				</div><!-- .section-container -->
+			</section><!-- #done -->
+
+		</div><!-- .main-container -->
+		
+		<!-- Overlay -->
+		<div class="overlay">
+			<div class="gradient-overlay background-blue-dark opacity-60"></div>
+		</div><!-- .overlay -->
+
+		<!-- Background Layer Image -->
+		<div class="background-layer cover-background image-1"></div>
+		
+<?php include ('footer.php') ?>
